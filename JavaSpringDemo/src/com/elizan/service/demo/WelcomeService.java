99629cb0bd14e3bb50bd5864f2955620dbb0be72
eelizanjr <eelizanjr@gmail.com>
2017-06-09 00:19:04 -0400

    Added demo

---
+++ JavaSpringDemo/src/com/elizan/service/demo/WelcomeService.java
@@ -0,0 +1,23 @@
+package com.elizan.service.demo;
+import java.util.*; 
+
+
+/*
+ * Service Later 
+ * Demonstrates separating the "logic" from the view 
+ */
+
+// returns an array list to be displayed in the view 
+public class WelcomeService {
+	
+	public List<String> getWelcome(String name) {
+		List<String> myWelcomeMessage = new ArrayList<>(); 
+		
+		// add data to the list 
+		myWelcomeMessage.add("Hello!" ); 
+		myWelcomeMessage.add(name); 
+		myWelcomeMessage.add(", welcome to the Spring course.");
+		
+		return myWelcomeMessage; 
+	}
+}
